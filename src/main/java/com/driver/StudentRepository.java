8be6449da19bb086b6164a4d3badd2358654d40e
GarimaM01 <garima.mahto@sakec.ac.in>
2023-03-05 15:27:27 +0530

    Classroom-Assignment-Attempt1

---
+++ src/main/java/com/driver/StudentRepository.java
@@ -0,0 +1,109 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class StudentRepository
+{
+    Map<String, Student> studentDB;
+    Map<String, Teacher> teacherDB;
+    Map<String, List<String>> teacherStudentDB;
+
+    public StudentRepository()
+    {
+        this.studentDB = new HashMap<>();
+        this.teacherDB = new HashMap<>();
+        this.teacherStudentDB = new HashMap<>();
+    }
+
+    public void addStudent(Student student)
+    {
+        studentDB.put(student.getName(), student);
+    }
+
+    public void addTeacher(Teacher teacher)
+    {
+        teacherDB.put(teacher.getName(), teacher);
+    }
+
+    public void addStudentTeacherPair(String student, String teacher)
+    {
+        if(teacherStudentDB.containsKey(teacher))
+        {
+            List<String> list = new ArrayList<>();
+            list.add(student);
+            teacherStudentDB.put(teacher, list);
+        }
+        else
+        {
+            List<String> list = new ArrayList<>();
+            list.add(student);
+            teacherStudentDB.put(teacher, list);
+        }
+        teacherDB.get(teacher).setNumberOfStudents(teacherStudentDB.get(teacher).size());
+    }
+
+    public Student getStudentByName(String name)
+    {
+        if(studentDB.containsKey(name))
+            return studentDB.get(name);
+        else return null;
+    }
+
+    public Teacher getTeacherByName(String name)
+    {
+        if(teacherDB.containsKey(name))
+            return teacherDB.get(name);
+        else return null;
+    }
+
+    public List<String> getStudentsByTeacherName(String teacher)
+    {
+        if(teacherStudentDB.containsKey(teacher))
+            return teacherStudentDB.get(teacher);
+        else return new ArrayList<>();
+    }
+
+    public List<String> getAllStudents()
+    {
+        return new ArrayList<>(studentDB.keySet());
+    }
+
+    public void deleteTeacherByName(String teacher)
+    {
+        if(teacherDB.containsKey(teacher))
+            teacherDB.remove(teacher);
+        if(teacherStudentDB.containsKey(teacher))
+        {
+            for(String student : teacherStudentDB.get(teacher))
+            {
+                if(studentDB.containsKey(student))
+                {
+                    studentDB.remove(student);
+                }
+            }
+        }
+        teacherStudentDB.remove(teacher);
+    }
+
+    public void deleteAllTeacher()
+    {
+        for(String teacher : teacherStudentDB.keySet())
+        {
+            if(teacherDB.containsKey(teacher)) teacherDB.remove(teacher);
+            for(String student : teacherStudentDB.get(teacher))
+            {
+                if(studentDB.containsKey(student))
+                {
+                    studentDB.remove(student);
+                }
+            }
+            teacherStudentDB.remove(teacher);
+        }
+    }
+}
